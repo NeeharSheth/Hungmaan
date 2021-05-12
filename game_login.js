1284eb145b37ff92e8e284b734b9de5be56f2157
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-05-12 13:25:36 +0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+function add_user(){
+    player1_name= document.getElementById("player1_input").value;
+    player2_name= document.getElementById("player2_input").value;
+    localStorage.setItem("player1",player1_name);
+    localStorage.setItem("player2",player2_name);
+    window.location="game_page.html";
+}
+
+function send(){
+    console.log("send works");
+}
