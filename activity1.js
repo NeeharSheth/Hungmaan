073f10dd9d0cf8e473b7fbd7a2cea5b93598b807
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-05-12 13:12:29 +0400

    Add files via upload

--- activity1.js
+++ activity1.js
@@ -8,3 +8,7 @@ document.getElementById("score_display").innerHTML= "Score: "+score;
 function score_save(){
 localStorage.setItem("Score",score);
 }
+
+function next(){
+    window.location="activity_2.html";
+}
